111270f46e72b5b26b13820fc0644aebdde1a325
Genestealer <Genestealer@users.noreply.github.com>
2018-05-19 20:41:11 +0100

    Chnaged platform and aligned to my other projects

--- src/LC_NodeMcuCode.cpp
+++ src/LC_NodeMcuCode.cpp
@@ -28,9 +28,9 @@
     DHT22 temperature-humidity sensor - GPIO pin 5 (NodeMCU Pin D1)
   Outputs:
     433Mhz Transmitter - GPIO pin 2 (NODEMCU Pin D4)
-    LED_NODEMCU - pin 16 (NodeMCU Pin D0)
-    LED_ESP - GPIO pin 2 (NodeMCU Pin D4) (Shared with 433Mhz TX)
-  ----------
+    LED_NODEMCU        - GPIO pin 16 (NodeMCU pin D0)
+    LED_ESP            - GPIO pin 2  (NodeMCU pin D4) (Shared with 433Mhz TX)
+    ----------
   Notes:
     NodeMCU lED lights to show MQTT conenction.
     ESP lED lights to show WIFI conenction.
@@ -50,34 +50,30 @@
 #include <private.h>         // Passwords etc not for github
 #include <ESP8266WiFi.h>     // ESP8266 core for Arduino https://github.com/esp8266/Arduino
 #include <PubSubClient.h>    // Arduino Client for MQTT https://github.com/knolleary/pubsubclient
-#include <RCSwitch.h>        // RF control lib, https://github.com/sui77/rc-switch
-#include <DHT.h>             // DHT Sensor lib, https://github.com/adafruit/DHT-sensor-library
-#include <Adafruit_Sensor.h> // Have to add for the DHT to work https://github.com/adafruit/Adafruit_Sensor
 #include <ESP8266mDNS.h>     // Needed for Over-the-Air ESP8266 programming https://github.com/esp8266/Arduino
 #include <WiFiUdp.h>         // Needed for Over-the-Air ESP8266 programming https://github.com/esp8266/Arduino
 #include <ArduinoOTA.h>      // Needed for Over-the-Air ESP8266 programming https://github.com/esp8266/Arduino
 #include <ArduinoJson.h>     // For sending MQTT JSON messages https://bblanchon.github.io/ArduinoJson/
+#include <RCSwitch.h>        // RF control lib, https://github.com/sui77/rc-switch
+#include <DHT.h>             // DHT Sensor lib, https://github.com/adafruit/DHT-sensor-library
+#include <Adafruit_Sensor.h> // Have to add for the DHT to work https://github.com/adafruit/Adafruit_Sensor
+
+
 // #include <IRremoteESP8266.h> //https://github.com/markszabo/IRremoteESP8266
-
-// DHT sensor parameters
-#define DHTPIN 5 // GPIO pin 5 (NodeMCU Pin D1)
-#define DHTTYPE DHT22
-
-// DHT sensor instance
-DHT dht(DHTPIN, DHTTYPE, 15);
 
 // WiFi parameters
 const char* wifi_ssid = secret_wifi_ssid; // Wifi access point SSID
 const char* wifi_password = secret_wifi_password; // Wifi access point password
-
-// 433Mhz transmitter parameters
-const int tx433Mhz_pin = 2; // GPIO pin 2 (NODEMCU Pin D4)
-const int setPulseLength = 305;
-
-// 433MHZ TX instance
-RCSwitch mySwitch = RCSwitch();
+int noWifiConnectionCount = 0;
+const int noWifiConnectionCountLimit = 5;
+int noWifiConnectionCountRebootCount = 0;
+const int noWifiConnectionCountRebootLimit = 5;
 
 // MQTT Settings
+WiFiClient espClient;
+PubSubClient mqttClient(espClient);
+char message_buff[100];
+long lastReconnectAttempt = 0; // Reconnecting MQTT - non-blocking https://github.com/knolleary/pubsubclient/blob/master/examples/mqtt_reconnect_nonblocking/mqtt_reconnect_nonblocking.ino
 const char* mqtt_server = secret_mqtt_server; // E.G. 192.168.1.xx
 const char* clientName = secret_clientName; // Client to report to MQTT
 const char* mqtt_username = secret_mqtt_username; // MQTT Username
@@ -85,59 +81,86 @@ const char* mqtt_password = secret_mqtt_password; // MQTT Password
 bool willRetain = true; // MQTT Last Will and Testament
 const char* willMessage = "offline"; // MQTT Last Will and Testament Message
 const int json_buffer_size = 256;
-
-// Subscribe
+int noMqttConnectionCount = 0;
+const int noMqttConnectionCountLimit = 5;
+// MQTT Subscribe
 const char* subscribeLightingGatewayTopic = secret_subscribeLightingGatewayTopic; // E.G. Home/LightingGateway/transmit
-// Publish
-const char* publishTemperatureTopic = secret_publishTemperatureTopic; // E.G. Home/Room/temperature
-const char* publishHumidityTopic = secret_publishHumidityTopic; // E.G. Home/Room/humidity
-
-const char* publishLastWillTopic = secret_publishLastWillTopic; // E.G. Home/LightingGateway/status"
-const char* publishSensorJsonTopic = secret_publishSensorJsonTopic;
-const char* publishStatusJsonTopic = secret_publishStatusJsonTopic;
-
-
-// MQTT instance
-WiFiClient espClient;
-PubSubClient mqttClient(espClient);
-char message_buff[100];
-long lastReconnectAttempt = 0; // Reconnecting MQTT - non-blocking https://github.com/knolleary/pubsubclient/blob/master/examples/mqtt_reconnect_nonblocking/mqtt_reconnect_nonblocking.ino
-
+// MQTT Publish
+const char* publishLastWillTopic = secret_publishLastWillTopic;              // MQTT last will
+const char* publishNodeStatusJsonTopic = secret_publishNodeStatusJsonTopic;  // State of the node
+const char* publishNodeHealthJsonTopic = secret_publishNodeHealthJsonTopic;  // Health of the node
 // MQTT publish frequency
 unsigned long previousMillis = 0;
-const long publishInterval = 60000; // Publish requency in milliseconds 60000 = 1 min
+const long publishInterval = 60000; // Publish frequency in milliseconds 60000 = 1 min
 
 // LED output parameters
 const int DIGITAL_PIN_LED_ESP = 2; // Define LED on ESP8266 sub-modual
 const int DIGITAL_PIN_LED_NODEMCU = 16; // Define LED on NodeMCU board - Lights on pin LOW
 
 
+// DHT sensor parameters
+#define DHTPIN 5 // GPIO pin 5 (NodeMCU Pin D1)
+#define DHTTYPE DHT22
+// DHT sensor instance
+DHT dht(DHTPIN, DHTTYPE, 15);
 
-// Setp the connection to WIFI and the MQTT Broker. Normally called only once from setup
+// 433MHZ TX instance
+RCSwitch mySwitch = RCSwitch();
+// 433Mhz transmitter parameters
+const int tx433Mhz_pin = 2; // GPIO pin 2 (NODEMCU Pin D4)
+const int setPulseLength = 305;
+
+// Setup the connection to WIFI. Normally called only once from setup
 void setup_wifi() {
   /* Explicitly set the ESP8266 to be a WiFi-client, otherwise, it by default,
      would try to act as both a client and an access-point and could cause
      network-issues with your other WiFi-devices on your WiFi-network. */
   WiFi.mode(WIFI_STA);
-
   // Connect to the WiFi network
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.print(wifi_ssid);
+  Serial.println("Connecting to " + String(wifi_ssid) + "...");
   WiFi.begin(wifi_ssid, wifi_password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(250);
-    Serial.print(".");
+  while ((WiFi.status() != WL_CONNECTED) && (noWifiConnectionCount < noWifiConnectionCountLimit)) {
+    delay(500);
+    noWifiConnectionCount = ++noWifiConnectionCount; //Increment the counter
+    Serial.print("Wifi connection attempt number: ");
+    Serial.println(noWifiConnectionCount);
   }
-  Serial.print("");
-  Serial.println("WiFi connected");
+  if (WiFi.status() != WL_CONNECTED) {
+    // If no wifi, fall-back to local mode with pre-set values.
+    Serial.println("WiFi not connected. Running in Local Mode!");
+  } else {
+    Serial.println("WiFi connected");
 
-  Serial.printf("RSSI: %d dBm\n", WiFi.RSSI());
-  Serial.print("Connected, IP address: ");
-  Serial.println(WiFi.localIP());
-  Serial.printf("Hostname: %s\n", WiFi.hostname().c_str());
+    Serial.printf("RSSI: %d dBm\n", WiFi.RSSI());
+    Serial.println("IP address: " + String(WiFi.localIP()));
+    Serial.printf("Hostname: %s\n", WiFi.hostname().c_str());
 
-  digitalWrite(DIGITAL_PIN_LED_NODEMCU, LOW); // Lights on LOW. Light the NodeMCU LED to show wifi connection.
+    digitalWrite(DIGITAL_PIN_LED_NODEMCU, LOW); // Lights on LOW. Light the NodeMCU LED to show wifi connection.
+  }
+}
+
+// If disconnected from Wifi, and attempt reconnection.
+// Code source: https://github.com/alukach/Amazon-Alexa-RF-Outlets-Integration/blob/master/wemos.ino
+bool reconnectWifi() {
+  Serial.println("Disconnected; Attempting reconnect to " + String(wifi_ssid) + "...");
+  WiFi.disconnect();
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(wifi_ssid, wifi_password);
+  // Output reconnection status info every second over the next 10 sec
+  for ( int i = 0; i < 10 ; i++ )  {
+    delay(500);
+    Serial.print("WiFi status = ");
+    if ( WiFi.status() == WL_CONNECTED ) {
+      Serial.println("Connected");
+      return true;
+    } else {
+      Serial.println("Disconnected");
+    }
+  }
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("Failure to establish connection after 10 sec. Returning to Local Mode");
+    return false;
+  }
 }
 
 // Setup Over-the-Air programming, called from the setup.
@@ -190,7 +213,7 @@ void transmit433Msg(int msgToSend) {
 }
 
 // Publish this nodes state via MQTT
-void publishNodeState() {
+void publishNodeHealth() {
   // Update status to online, retained = true - last will Message will drop in if we go offline
   mqttClient.publish(publishLastWillTopic, "online", true);
   // Gather data
@@ -212,10 +235,15 @@ void publishNodeState() {
   Serial.println(""); // Add new line as prettyPrintTo leaves the line open.
   char data[json_buffer_size];
   root.printTo(data, root.measureLength() + 1);
-  if (!mqttClient.publish(publishStatusJsonTopic, data, true)) // retained = true
-    Serial.print(F("Failed to publish JSON Status to [")), Serial.print(publishStatusJsonTopic), Serial.print("] ");
+  if (!mqttClient.publish(publishNodeHealthJsonTopic, data, true)) // retained = true
+    Serial.println("Failed to publish JSON Node Health to [" + String(publishNodeHealthJsonTopic) + "]");
   else
-    Serial.print(F("JSON Status Published [")), Serial.print(publishStatusJsonTopic), Serial.println("] ");
+    Serial.println("JSON Node Health Published [" + String(publishNodeHealthJsonTopic) + "]");
+}
+
+// Subscribe to MQTT topics
+void mqttSubscribe() {
+  mqttClient.subscribe(subscribeLightingGatewayTopic);
 }
 
 /*
@@ -224,22 +252,19 @@ void publishNodeState() {
   Based on example from 5ace47b Sep 7, 2015 https://github.com/knolleary/pubsubclient/blob/master/examples/mqtt_reconnect_nonblocking/mqtt_reconnect_nonblocking.ino
 */
 boolean mqttReconnect() {
+  Serial.println("mqttReconnect");
   // Call on the background functions to allow them to do their thing
   yield();
   // Attempt to connect
   if (mqttClient.connect(clientName, mqtt_username, mqtt_password, publishLastWillTopic, 0, willRetain, willMessage)) {
     Serial.print("Attempting MQTT connection...");
     // Publish node state data
-    publishNodeState();
+    publishNodeHealth();
     // Resubscribe to feeds
-    mqttClient.subscribe(subscribeLightingGatewayTopic);
+    mqttSubscribe();
     Serial.println("Connected to MQTT server");
-  }
-  else
-  {
-    Serial.print("Failed MQTT connection, rc=");
-    Serial.print(mqttClient.state());
-    Serial.println(" try again in 1.5 seconds");
+  } else {
+    Serial.println("Failed MQTT connection, rc=" + String(publishNodeStatusJsonTopic) + "] ");
   }
   return mqttClient.connected(); // Return connection state
 }
@@ -250,6 +275,8 @@ boolean mqttReconnect() {
   its connection, it attempts to reconnect every 5 seconds
   without blocking the main loop.
   Called from main loop.
+  If MQTT connection fails after x attempts it tries to reconnect wifi
+  If wifi connections fails after x attempts it reboots the esp
 */
 void checkMqttConnection() {
   if (!mqttClient.connected()) {
@@ -260,12 +287,36 @@ void checkMqttConnection() {
       lastReconnectAttempt = now;
       // Attempt to reconnect
       if (mqttReconnect()) {
+        // We are connected.
         lastReconnectAttempt = 0;
+        noMqttConnectionCount = 0;
+        noWifiConnectionCountRebootCount = 0;
+        digitalWrite(DIGITAL_PIN_LED_ESP, LOW); // Lights on LOW
+      } else  {
+        // Connection to MQTT failed.
+        // If no connection after x attempts, then reconnect wifi, if no connection after x attempts reboot.
+        noMqttConnectionCount = ++noMqttConnectionCount; //Increment the counter
+        Serial.println("MQTT connection attempt number: " + String(noMqttConnectionCount));
+        if (noMqttConnectionCount > noMqttConnectionCountLimit) {
+          // Max MQTT connection attempts reached, reconnect wifi.
+          noMqttConnectionCount = 0; // Reset MQTT connection attempt counter.
+          Serial.println("MQTT connection count limit reached, reconnecting wifi");
+          // Try to reconnect wifi, if this fails after x attemps then reboot.
+          if (!reconnectWifi()) {
+            noWifiConnectionCountRebootCount = ++noWifiConnectionCountRebootCount;
+            Serial.println("Wifi connection attempt number: " + String(noWifiConnectionCountRebootCount));
+            if (noWifiConnectionCountRebootCount > noWifiConnectionCountRebootLimit) {
+              Serial.println("Wifi re-connection count limit reached, reboot esp");
+              // Reboot ESP
+              ESP.restart();
+            }
+          }
+        }
       }
+
+
     }
   } else {
-    // We are connected.
-    digitalWrite(DIGITAL_PIN_LED_ESP, LOW); // Lights on LOW
     //Call on the background functions to allow them to do their thing.
     yield();
     // Client connected: MQTT client loop processing
@@ -274,15 +325,15 @@ void checkMqttConnection() {
 }
 
 // MQTT Publish
-void mqttPublishData() {
+void mqttPublishStatusData() {
   // Only run when publishInterval in milliseonds exspires
   unsigned long currentMillis = millis();
   if (currentMillis - previousMillis >= publishInterval) {
     previousMillis = currentMillis; // Save the last time this ran
-    // Check conenction to MQTT server
+    // Check connection to MQTT server
     if (mqttClient.connected()) {
       // Publish node state data
-      publishNodeState();
+      publishNodeHealth();
 
       // JSON data method
       StaticJsonBuffer<json_buffer_size> jsonBuffer;
@@ -294,44 +345,26 @@ void mqttPublishData() {
       Serial.println(""); // Add new line as prettyPrintTo leaves the line open.
       char data[json_buffer_size];
       root.printTo(data, root.measureLength() + 1);
-      if (!mqttClient.publish(publishSensorJsonTopic, data))
-        Serial.print(F("Failed to publish JSON sensor data to [")), Serial.print(publishSensorJsonTopic), Serial.print("] ");
+      if (!mqttClient.publish(publishNodeStatusJsonTopic, data, true))  //Retain data.
+        Serial.println("Failed to publish JSON sensor data to [" + String(publishNodeStatusJsonTopic) + "]");
       else
-        Serial.print(F("JSON Sensor data published to [")), Serial.print(publishSensorJsonTopic), Serial.println("] ");
-      Serial.println("JSON Sensor Published");
-
-      // Old legacy method
-      // Grab the current state of the sensor
-      String strTemp = String(dht.readTemperature()); //Could use String(dht.readTemperature()).c_str()) to do it all in one line
-      if (!mqttClient.publish(publishTemperatureTopic, String(dht.readTemperature()).c_str())) // Convert dht.readTemperature() to string object, then to char array.
-        Serial.print(F("Failed to published to [")), Serial.print(publishTemperatureTopic), Serial.print("] ");
-      else
-        Serial.print(F("Temperature published to [")), Serial.print(publishTemperatureTopic), Serial.println("] ");
-
-      String strHumi = String(dht.readHumidity());
-      if (!mqttClient.publish(publishHumidityTopic, strHumi.c_str()))
-        Serial.print(F("Failed to humidity to [")), Serial.print(publishHumidityTopic), Serial.print("] ");
-      else
-        Serial.print(F("Humidity published to [")), Serial.print(publishHumidityTopic), Serial.println("] ");
+        Serial.println("JSON Sensor data published to [" + String(publishNodeStatusJsonTopic) + "] ");
     }
   }
 }
+
 
 // MQTT payload
 // to transmit via out gateway
 void mqttcallback(char* topic, byte* payload, unsigned int length) {
   //If you want to publish a message from within the message callback function, it is necessary to make a copy of the topic and payload values as the client uses the same internal buffer for inbound and outbound messages:
   //http://www.hivemq.com/blog/mqtt-client-library-encyclopedia-arduino-pubsubclient/
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
+  Serial.println("Message arrived [" + String(topic) + "] ");
   for (int i = 0; i < length; i++) {
     Serial.print((char)payload[i]);
   }
   Serial.println();
-
-
-  // create character buffer with ending null terminator (string)
+  // Create character buffer with ending null terminator (string)
   int i = 0;
   for (i = 0; i < length; i++) {
     message_buff[i] = payload[i];
@@ -340,7 +373,6 @@ void mqttcallback(char* topic, byte* payload, unsigned int length) {
   // Check the value of the message
   String msgString = String(message_buff);
   Serial.println(msgString);
-
   // Check the message topic
   String srtTopic = topic;
 
@@ -354,16 +386,11 @@ void mqttcallback(char* topic, byte* payload, unsigned int length) {
   }
 }
 
-void setup() {
-  // Initialize pins
-  pinMode(DIGITAL_PIN_LED_NODEMCU, OUTPUT);
-  pinMode(DIGITAL_PIN_LED_ESP, OUTPUT);
+// Custom setup for this program.
+void customSetup() {
   // Initialize pin start values
   digitalWrite(DIGITAL_PIN_LED_NODEMCU, LOW); // Lights on HIGH
   digitalWrite(DIGITAL_PIN_LED_ESP, HIGH); // Lights on LOW
-  // Set serial speed
-  Serial.begin(115200);
-  Serial.println("Setup Starting");
 
   // Setup 433Mhz Transmitter
   pinMode(tx433Mhz_pin, OUTPUT);
@@ -373,6 +400,21 @@ void setup() {
   mySwitch.setPulseLength(setPulseLength);
   // Optional set protocol (default is 1, will work for most outlets)
   mySwitch.setProtocol(1);
+
+  // Init temperature and humidity sensor
+  dht.begin();
+
+}
+void setup() {
+  // Set serial speed
+  Serial.begin(115200);
+  Serial.println("Setup Starting");
+  // Initialize pins
+  pinMode(DIGITAL_PIN_LED_NODEMCU, OUTPUT);
+  pinMode(DIGITAL_PIN_LED_ESP, OUTPUT);
+  // Initialize pin start values
+  digitalWrite(DIGITAL_PIN_LED_NODEMCU, LOW); // Lights on HIGH
+  digitalWrite(DIGITAL_PIN_LED_ESP, HIGH); // Lights on LOW
 
   // Call on the background functions to allow them to do their thing
   yield();
@@ -389,6 +431,8 @@ void setup() {
   mqttClient.setCallback(mqttcallback);
   // Call on the background functions to allow them to do their thing
   yield();
+  // Setup for this project.
+  customSetup();
   Serial.println("Setup Complete");
 }
 
@@ -396,17 +440,16 @@ void setup() {
 void loop() {
   // Call on the background functions to allow them to do their thing.
   yield();
-  // First check if we are connected to the MQTT broker
-  checkMqttConnection();
-  // Call on the background functions to allow them to do their thing.
-  yield();
-  // Publish MQTT
-  mqttPublishData();
-  // Call on the background functions to allow them to do their thing.
-  yield();
   // Check for Over The Air updates
   ArduinoOTA.handle();
-
+  // Call on the background functions to allow them to do their thing.
+  yield();
+  // Check if we are connected to the MQTT broker
+  checkMqttConnection();
+  // Publish MQTT
+  mqttPublishStatusData();
+  // Call on the background functions to allow them to do their thing.
+  yield();
   // Deal with millis rollover, hack by resetting the esp every 48 days
   if (millis() > 4147200000)
     ESP.restart();
